3861dfe7fc8d91800e0978d75aca81060bc199a6
Alf Eaton <hubgit@users.noreply.github.com>
2016-02-25 22:49:00 +0000

    Create csv_read.php

---
+++ csv_read.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @param string   $path The path to the file to be read
+ * @param string[] $keys An optional array of column names
+ */
+function csv_read($path, $keys = []) {
+  $input = fopen($path, 'r'); // open the file
+  $keys = $keys ?: fgetcsv($input); // use the keys provided, or read them from the first row
+  $columns = count($keys); // count the expected number of columns
+  while (($row = fgetcsv($input)) !== false) { // parse each line of the file
+    $values = array_pad($row, $columns, null); // ensure that each column has a value
+    yield array_combine($keys, $values); // yield an associative array
+  }
+  fclose($input); // close the file
+}
