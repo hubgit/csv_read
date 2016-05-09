714676bb3baa7f927156c15201a8f9bc200ba89f
Alf Eaton <hubgit@users.noreply.github.com>
2016-05-09 11:23:44 +0100

    Fix parameters

--- csv_read.php
+++ csv_read.php
@@ -9,7 +9,7 @@ function csv_read($path, $keys = [], $delimiter = ',') {
   $input = fopen($path, 'r'); // open the file
   $keys = $keys ?: fgetcsv($input, 0, $delimiter); // use the keys provided, or read them from the first row
   $count = count($keys); // count the expected number of columns
-  while (($row = fgetcsv($input, $delimiter)) !== false) { // parse each line of the file
+  while (($row = fgetcsv($input, 0, $delimiter)) !== false) { // parse each line of the file
     $values = array_pad($row, $count, null); // ensure that each column has a value
     yield array_combine($keys, $values); // yield an associative array
   }
