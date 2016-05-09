31f40aebc14108d0efcb63658db4ddba89fb545a
Alf Eaton <hubgit@users.noreply.github.com>
2016-05-09 11:19:45 +0100

    Add delimiter parameter

--- csv_read.php
+++ csv_read.php
@@ -4,11 +4,11 @@
  * @param string   $path The path to the file to be read
  * @param string[] $keys An optional array of column names
  */
-function csv_read($path, $keys = []) {
+function csv_read($path, $keys = [], $delimiter = ',') {
   $input = fopen($path, 'r'); // open the file
-  $keys = $keys ?: fgetcsv($input); // use the keys provided, or read them from the first row
+  $keys = $keys ?: fgetcsv($input, 0, $delimiter); // use the keys provided, or read them from the first row
   $count = count($keys); // count the expected number of columns
-  while (($row = fgetcsv($input)) !== false) { // parse each line of the file
+  while (($row = fgetcsv($input, $delimiter)) !== false) { // parse each line of the file
     $values = array_pad($row, $count, null); // ensure that each column has a value
     yield array_combine($keys, $values); // yield an associative array
   }
